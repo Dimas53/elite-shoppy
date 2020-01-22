2606b6e31a5419955393fac78a0d0670d08daccf
Dimas53 <dima.savcenco@gmail.com>
2020-01-22 20:25:00 +0200

    commit first

---
+++ src/reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {combineReducers} from "redux";
+import {connectRouter} from "connected-react-router";
+
+import products from "./products";
+
+export default history => combineReducers({
+  products,
+  router: connectRouter(history)
+})
