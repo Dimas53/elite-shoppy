d08bb689468fcae143591d1f6f9c65c191b72e82
Dimas53 <dima.savcenco@gmail.com>
2020-02-16 22:56:15 +0200

    commit category

--- src/components/categories/categories.js
+++ src/components/categories/categories.js
@@ -1,10 +1,33 @@
 import React from "react";
 import {connect} from "react-redux"
 import {getCategories} from "../../selectors";
+import {Link} from "react-router-dom";
+import "./categories.css"
 
-const Categories = () => {
+const Categories = ({categories}) => {
+  const renderCategory =(category, index) => {
+    return (
+      <Link
+        to={`/categories/${category.id}`}
+        className="categories_item"
+        key={index}
+      >
+        {/*<i className="fa fa-caret-right"></i>*/}
+        <span> > </span>
+        {category.name}
+
+      </Link>
+    )
+  }
   return (
-    <div>categories</div>
+    <div className="categories">
+      <div className="categories_name card_btn">
+        <h4>All Categories</h4>
+      </div>
+      <div className="categories_list">
+        {categories.map((category, index) => renderCategory(category, index))}
+      </div>
+    </div>
   )
 }
 
